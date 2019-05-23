80ed418124e58c8dc98eb2d9dae1d25acd6cc80a
Viet-Dung Truong <deivatko@gmail.com>
2019-05-23 19:37:20 +0200

    adding httpresponse

--- TruongWebShop/firstDjandgoWebsite/polls/views.py
+++ TruongWebShop/firstDjandgoWebsite/polls/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. Your're at the polls index.")
 
 # Create your views here.
 from django.http import HttpResponse
